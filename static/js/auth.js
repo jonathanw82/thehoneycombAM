2de85d4bf5016d0707489382aff4e374fafe7286
JonathanW82 <Jonwheway@live.co.uk>
2020-09-15 12:44:18 +0100

    added javascript to help with login/authentication screen resizing on screens between 425 and 769

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,5 +1,19 @@
-$(window).resize(function() {
-    // This will execute whenever the window is resized
-    $(window).height(768); // New height
-    $(window).width(820); // New width
-  });
+
+/*  On document load this code looks at the window size and looks to see 
+if it is greater that 425px wide, if so remove one class and add another
+and vice versa. */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+var screensize = document.documentElement.clientWidth;
+var windowSize = document.getElementById('login-boxsize');
+if (screensize > 426 && screensize < 769){
+    
+    windowSize.classList.remove('col-md-6');
+    windowSize.classList.add('col-md-7');
+}
+else{
+    windowSize.classList.remove('col-md-7');
+    windowSize.classList.add('col-md-6');
+}
+});
