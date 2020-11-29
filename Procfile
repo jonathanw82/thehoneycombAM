fecff26f17bf5c206c8ea5a64dd4c3567318aa1d
jonathanw82 <jonwhewaycode@outlook.com>
2020-11-29 18:32:17 +0000

    ready to deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn thehoneycombam.wsgi:application
--- thehoneycombam/settings.py
+++ thehoneycombam/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('THCAM_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['honeycomb-am.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
