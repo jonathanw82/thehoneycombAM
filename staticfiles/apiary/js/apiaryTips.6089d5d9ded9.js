9b4bb9966b99565ec858b57c21d3ab81e87ec13f
jonathanw82 <jonwhewaycode@outlook.com>
2020-12-01 13:49:13 +0000

    collected the static

---
+++ staticfiles/apiary/js/apiaryTips.6089d5d9ded9.js
@@ -0,0 +1,119 @@
+// Apiary tips for each month
+
+jan = `<span><h3>Tips for January</h3><p><ul>
+<li>Check entrances are clear so bees can fly in and out</li>
+<li>Heft your hives, add slab fondant if needed</li>
+<li>Clean and prepare spare equipment, buy new at Winter Sales<br>
+(Supers, Nuc boxes, Replacement brood frames, Spare brood boxes, Queen excluders)</li>
+<li>Plan your activities for the new year<br>
+(Increase, maximise honey crop, queen replacement, queen rearing, new techniques)
+</li>
+</ul></p></span>`;
+
+feb = `<span><h3>Tips for February</h3><p><ul>
+<li>Watch the bees for pollen intake
+<br>(Crocus, Salix, Viburnum, Blackthorn….)</li>
+<li>Heft hives, consider feeding with syrup @1.1 and pollen patties</li>
+<li>Tidy up the apiary whilst the bees are not flying much</li>
+<li>On a nice day, open the hives, find and mark the Queens</li>
+<li>Organise your swarm collection kit for quick access</li>
+<li>Collect and dry smoker fuel</li>
+<li>Clean (& repaint) varroa monitoring boards</li>
+</ul></p></span>`;
+
+mar = `<span><h3>Tips for March</h3><p><ul>
+<li>Ensure beekeeping tools and clothing are clean and ready for use.</li>
+<li>Arrange a bucket of washing soda to clean tools and gloves between hives.</li>
+<li>First quick inspection on a warm day.<br>
+Look for Q, eggs & brood, sufficient stores (10 lbs), diseases.</li>
+<li>Remove mouse guards and winter insulation</li>
+<li>Start assessing mite fall every month (less than 4/day is OK)</li>
+</ul></p></span>`;
+
+apr = `<span><h3>Tips for April</h3><p><ul>
+<li>First full inspections when warm enough (Shirt sleeve weather)</li>
+<li>The 5 concerns of all inspections:</li>
+<ol>
+<li>Check for eggs & healthy brood</li>
+<li>Space to lay eggs and expand colony</li>
+<li>Stores ‘til next inspection</li>
+<li>Building up well</li>
+<li>Diseases</li>
+</ol>
+<li>Start weekly inspections</li>
+<li>Assess temperament for later queen replacement</li>
+<li>Remove old brood combs and replace with new</li>
+<li>Consider doing a Shook Swarm or Bailey Frame Change procedure</li>
+<li>If 7 frames of brood, put on super(s) with Queen excluder</li>
+<li>Watch out for Queen cells preparatory to swarming.  If Q cells present:</li>
+<li>Read and consider: <a href="https://www.wbka.com/wp-content/uploads/2013/06/There-Are-Queen-Cells-In-My-Hive-WBKA-WAG.pdf" title="There_are_Queen_Cells_in_my_Hive">[Queen Cells - WBKA]</a></li>
+<li>Consider making up a small nuc to raise a fresh Queen</li>
+<li>Consider doing a Pagden split or Demaree stack<br>
+(Important to keep Queen laying but stop colony swarming)</li>
+</ul></p></span>`;
+
+may =`<span><h3>Tips for May</h3><p><ul>
+<li>Continue weekly inspection looking for Queen cells</li>
+<li>If 7 frames of brood, put on super(s) with Queen excluder</li>
+<li>If Q cells present</li>
+<ul>
+<li>Read and consider: <a href="https://www.wbka.com/wp-content/uploads/2013/06/There-Are-Queen-Cells-In-My-Hive-WBKA-WAG.pdf" title="There_are_Queen_Cells_in_my_Hive">[Queen Cells - WBKA]</a></li>
+<li>Consider making up a small nuc to raise a fresh Queen</li>
+<li>Consider using a Snelgrove split or Demaree stack<br>
+(Important to keep Queen laying but stop colony swarming)</li>
+</ul></p></span>`;
+
+jun = `<span><h3>Tips for June</h3><p><ul>
+<li>Continue with weekly inspections, watching for Queen cells</li>
+<li>Continue swarm control measures</li>
+<li>Collect swarms (get help if needed)</li>
+<li>Ensure sufficient supers for main flow</li>
+<li>Extract full supers</li>
+<li>Mark (and clip?) new queens once sealed worker brood present</li>
+</ul></p></span>`;
+
+jul = `<span><h3>Tips for July</h3><p><ul>
+<li>Continue weekly inspections, watching for Queen cells</li>  
+<li>Watch for swarms</li>
+<li>Add supers (up to 1 a week)</li>
+<li>Prepare extraction equipment and storage containers</li>
+<li>Move nucs into full hives, or combine with current colonies, when ready</li>
+</ul></p></span>`;
+
+agu = `<span><h3>Tips for August</h3><p><ul>
+<li>Clear and remove full supers for extraction.</li>
+<li>Extract, filter and store honey (Check water content)</li>
+<li>Clean wet supers on hive, possibly use Certan against wax moth.</li>
+<li>Clean or replace old, dirty super frames.</li>
+<li>Move old brood frames to the edge of hive ready for replacement.</li>
+<li>Ensure the colony is left with space to hold ~35 lbs honey for the Winter.</li>
+<li> If you plan to use Apiguard, now is the moment.</li>
+</ul></p></span>`;
+
+sep = `<span><h3>Tips for September</h3><p><ul>
+<li>Feed colonies that need it<br>
+(Deep National, 14x12, wooden hive weighs 15kg empty, requires 20kg stores)</li>
+<li>Reduce hive entrance size to deter wasps.</li>
+<li>Full brood disease inspection, shake off all bees.</li>
+<li>Reduce apiary to planned number of colonies.</li>
+<li>Reduce inspections to fortnightly.</li>
+</ul></p></span>`;
+
+oct = `<span><h3>Tips for October</h3><p><ul>
+<li>Add mouse guards and protect from wood peckers etc.</li>
+<li>Check stability of the hives, strap down ready for winter gales.</li>
+<li>Heft hives, final syrup @2.1 feed if needed.</li>
+</ul></p></span>`;
+
+nov = `<span><h3>Tips for November</h3><p><ul>
+<li>Clean up apiary, tools and equipment.</li>
+<li>Consider Joining a study group & prepare together for a BBKA exam in March.</li>
+<li>Leave bees alone - Go on holiday!</li>
+</ul></p></span>`;
+
+dec = `<span><h3>Tips for December</h3><p><ul>
+<li>Relax!</li>
+<li>On a nice day, treat colonies with Oxalic Acid vapour, after sacrificing any extant capped brood.</li>
+<li>Monitor total mite fall (~2-300), in 3 days time, treat again if still too high.</li>
+<li>Insulate hive roof.</li>
+</ul></p></span>`;
