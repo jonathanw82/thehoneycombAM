91add3b44d16131c4e154be643ffe5583753afee
jonathanw82 <59360164+jonathanw82@users.noreply.github.com>
2021-01-11 15:28:54 +0000

    updated allowed hosts in setting

--- apismetrics/settings.py
+++ apismetrics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('APM_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['honeycomb-am.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['apis-metrics.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
