fc5dc3b5224439a58b8a9c9384252a3d76b268bf
jonathanw82 <jonwhewaycode@outlook.com>
2021-01-19 13:03:39 +0000

    added styling site wide also sorted access to medical records and added modals for better ux on both hive info and hive history

--- hives/static/hives/js/hiveDocs.js
+++ hives/static/hives/js/hiveDocs.js
@@ -1,3 +1,15 @@
+/*
+ {% if doc.image1 or doc.image2 %}
+<input class="btn btn-secondary showbutton hive-doc-buttons" type="button" id="button{{doc.pk}}"
+    onclick="show_images('image{{doc.pk}}', 'button{{doc.pk}}', 'buttonhide{{doc.pk}}')"
+    value="Images"></input>
+<input class="btn btn-secondary hidebutton hive-doc-buttons" type="button"
+    id="buttonhide{{doc.pk}}"
+    onclick="hide_images('image{{doc.pk}}', 'button{{doc.pk}}', 'buttonhide{{doc.pk}}')"
+    value="Hide"></input>
+{% endif %} 
+*/
+
 function show_images(image_pk, button_pk, button2_pk) {
     let images = document.getElementById(image_pk);
     images.classList.add("show-images");
@@ -20,3 +32,20 @@ function hide_images(image_id, button_pk, button2_pk) {
     but2.classList.remove("showbutton");
     but2.classList.add("hidebutton");
 }
+
+/*
+<div class="hide-images" id="image{{doc.pk}}">
+{% if doc.image1 %}
+<a href="{{ doc.image1.url }}" target="_blank">
+    <img class="imageondoc" src="{{ doc.image1.url }}"
+        alt="Oops lost Image1! {{ doc.image1.name }}">
+</a>
+{% endif %}
+{% if doc.image2 %}
+<a href="{{ doc.image2.url }}" target="_blank">
+    <img class="imageondoc" src="{{ doc.image2.url }}"
+        alt="Oops lost Image2!  {{ doc.image2.name }}">
+</a>
+{% endif %}
+</div></a>
+*/
