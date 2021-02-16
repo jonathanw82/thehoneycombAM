0034c655680915ed0a8fdcd5f9a736db0eec16f0
jonathanw82 <jonwhewaycode@outlook.com>
2021-02-16 15:51:38 +0000

    Still having trouble with collecting static files

--- apismetrics/settings.py
+++ apismetrics/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = ['apis-metrics.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
